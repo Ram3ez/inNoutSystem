5ee84cd11fcb7f46080004e9086bab38830ba496
Ram3ez <rameez12m@gmail.com>
2026-05-11 18:19:09 +0530

    moveScript

---
+++ moveScript.sh
@@ -0,0 +1,6 @@
+# Move to your project root
+# Copy the public folder (images, etc.)
+cp -r public .next/standalone/
+
+# Copy the static folder (generated JS/CSS)
+cp -r .next/static .next/standalone/.next/
